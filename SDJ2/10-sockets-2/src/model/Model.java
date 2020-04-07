1fc753d92823dd366a44c48b99c9ffc2ca63732e
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2020-04-07 15:16:46 +0200

    session 11 init

---
+++ SDJ2/10-sockets-2/src/model/Model.java
@@ -0,0 +1,10 @@
+package model;
+
+import utility.UnnamedPropertyChangeSubject;
+
+public interface Model extends UnnamedPropertyChangeSubject
+{
+  public Student getStudentByStudyNumber(String studyNumber) throws Exception;
+  public Student getStudentByName(String name) throws Exception;
+  public void addStudent(Student student) throws Exception;
+}
---
+++ SDJ2/10-sockets-2/src/network/NetworkType.java
@@ -0,0 +1,6 @@
+package network;
+
+public enum NetworkType
+{
+  NUMBER, NAME, STUDENT, ERROR;
+}
